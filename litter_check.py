0bebe6e6d6deb284d66bb945e4247ce0556515ec
Alejandra Lopez <alemarlo1992@gmail.com>
2020-08-12 19:45:22 -0700

    Updated phone numbers

--- litter_check.py
+++ litter_check.py
@@ -9,7 +9,7 @@ from twilio.base.exceptions import TwilioRestException
 import logging
 
 
-def puppy_alert():
+def puppy_alert(phone):
     """Send text when puppy is availble using Twilio API."""
     account_sid = environ['TWILIO_ACCOUNT_SID']
     auth_token = environ['TWILIO_AUTH_TOKEN']
@@ -21,7 +21,7 @@ def puppy_alert():
                     .create(
                             body='Puppy is availble! go to https://gailsdoodles.com/current-litters',
                             from_=TWILIO_NUMBER,
-                            to='+16192895400'
+                            to=phone
                      )
     print(message.sid)
 
@@ -35,28 +35,24 @@ def checking_pup():
         mom = environ.get("MOM", "Holly")
         print(f"checking with DAD {dad} and Mom: {mom}")
         headings = []
+        phones = ['+16192895400', '+16193075064', '+14084441501']
 
         get_page = requests.get('https://gailsdoodles.com/current-litters')
-        print("After get_page")
+
         page_details = BeautifulSoup(get_page.text, 'html.parser')
-        print("After get_details:")
+
         for headlines in page_details.find_all("h1"):
             headings.append(headlines.text.strip())
 
 
         for h1 in headings:
-            print("Each h1:", h1, type(h1), len(h1))
-            print("Checking Mom:",mom, h1 == mom)
-            print("Checking Dad:",dad, h1 == dad)
-
             if h1 == mom or h1 == dad: 
-                print("There was a match with heading:", h1)
                 available = True 
-        print("After headings", available)
+
         if available == True:
-            print("Puppy available")
-            # puppy_alert() #send text messege notification
+            for phone in phones: 
+                puppy_alert(phone) #send text messege notification
             break
         else: 
-            time.sleep(10) #sleep for 3 min 
+            time.sleep(180) #sleep for 3 min 
 
